987cdd53b0a9737305b72b26a79d0d532fd6a8db
zingbretsen <zach@zingbretsen.com>
2014-04-11 01:35:16 -0400

    cleaning up
    removing redundant lines, clarifying comments/variable names

--- fb_likes.r
+++ fb_likes.r
@@ -45,7 +45,8 @@ for (i in seq(my_friends_info$username)){
   }  
 }
 
-# I don't remember if this is necessary or not
+# Gets rid of the stupid leading NA
+# I'm sorry
 good <- which(!is.na(like_intersection_length))
 like_intersection_length <- like_intersection_length[good]
 
@@ -60,22 +61,23 @@ for (i in length(mat[1,])){
 }
 
 # Gets rid of unconnected people
-singletons <- which(rowSums(mat)!=0)
-mat <- mat[singletons,singletons]
+connected <- which(rowSums(mat)!=0)
+mat <- mat[connected,connected]
+my_friends_info <- my_friends_info[connected,]
 network <- graph.adjacency(mat, mode="undirected", weighted=TRUE)
 
 # Keeps the biggest connected cluster
 cl <- clusters(network)
 gc <- which(cl$membership == 1)
 mat <- mat[gc, gc]
+my_friends_info <- my_friends_info[gc,]
 
 # The rest is pretty much straight from Pablo's script
 # Except note the weighted flag in the adjacency graph call
 network <- graph.adjacency(mat, mode="undirected", weighted=TRUE)
-layout <- layout.drl(network,options=list(simmer.attraction=0))
 fc <- fastgreedy.community(network) ## communities / clusters
 set.seed(123)
-l <- layout.fruchterman.reingold(network, niter=1000, coolexp=0.5) ## layout
+l <- layout.fruchterman.reingold(network, niter=1000, coolexp=0.5, circular=TRUE) ## layout
 d <- data.frame(l); names(d) <- c("x", "y")
 d$cluster <- factor(fc$membership)
 
@@ -89,11 +91,7 @@ central.nodes <- lapply(communities(fc), function(x) x[which.max(d$degree[x])])
 central.names <- fc$names[unlist(central.nodes)] ## names of central nodes
 ## within each cluster
 
-## we can also add the labels to the plot
-# labels <- c("Dartmouth", "Home", "Phi Tau", "PBS/Uppver Valley", "Veronica's friends", "Others")
 d$label <- NA
-# d$label[unlist(central.nodes)] <- labels
-# d$label <- unlist(fc$names)
 
 ## let's put it all together, with some final touches on how points look
 p <- ggplot(d, aes(x=x, y=y, color=cluster))
